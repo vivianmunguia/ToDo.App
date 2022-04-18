535b4ded5c8ff469a12d98379284999c29ce198f
Vivian Munguía <andy_munguia@hotmail.com>
2022-04-17 20:33:48 -0700

    Repository initialization

---
+++ ToDo.App/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ToDo.App.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDo.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new HomePage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
